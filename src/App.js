76361a03ed672aeac9bc91b0fb5fe88d61d8aac5
Heike Kasper <heike.kasper@kadelo.de>
2022-08-17 10:32:59 +0200

    install router dom and added link to home

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Header from "./Components/Header/Header";
 import "./fonts/PokemonSolidNormal-xyWR.ttf";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
